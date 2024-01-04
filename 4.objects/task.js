efb28d6d08d5cc823fe4ce7097f0c5b508575200
Your Name <katy2606@list.ru>
2024-01-04 21:15:23 +0500

    task4

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,33 @@
 function Student(name, gender, age) {
-  
+    this.name = name
+    this.gender = gender
+    this.age = age
+    this.marks = []
 }
 
+new Student("Василиса", "женский", 19)
+new Student("Артём", "мужской", 25)
+
 Student.prototype.setSubject = function (subjectName) {
-  
+    this.subject = subjectName;
 }
 
 Student.prototype.addMarks = function (...marks) {
-  
+    if(marks.length){
+        this.marks = marks;
+    }
 }
 
 Student.prototype.getAverage = function () {
-  
+    if (this.marks.length){
+        return this.marks.reduce((m, mark) => m + mark, 0) / this.marks.length;
+    }else{
+        return 0;
+    }
 }
 
 Student.prototype.exclude = function (reason) {
-  
+    delete this.subject
+    delete this.marks
+    this.excluded = reason
 }
