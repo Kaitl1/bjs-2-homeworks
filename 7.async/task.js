75528ea83e037a93a35e596dab90578c73a6d159
Your Name <katy2606@list.ru>
2024-02-19 12:43:12 +0500

    qwe

--- 7.async/task.js
+++ 7.async/task.js
@@ -1,38 +1,84 @@
+class Ring{
+    constructor(callback, time, canCall) {
+        this.callback = callback
+        this.time = time
+        this.canCall = canCall
+    }
+
+    getTime()
+    {
+        return this.time;
+    }
+
+    getCanCall()
+    {
+        return this.canCall;
+    }
+
+    setCanCall(canCall)
+    {
+        this.canCall = canCall;
+    }
+}
+
 class AlarmClock {
     constructor() {
         this.alarmCollection = []
         this.intervalId = null
     }
 
-    addClock(time, func) {
-        if (!time||!func) {
+    addClock(time, callback) {
+        if (!time||!callback) {
             throw new Error('Отсутствуют обязательные аргументы')
         }
-        console.log(this.alarmCollection)
-        if (this.alarmCollection.find(time)) {
-            console.warn('Уже присутствует звонок на это же время')
+        this.alarmCollection.forEach((ring) => {
+            if (ring.time === time) {
+                console.log('Уже присутствует звонок на это же время')
+            }
         }
-        this.alarmCollection.push({'callback': func, 'time': time, 'canCall': true})
+    )
+        for (let i = 0; i < this.alarmCollection.length; i++) {
+            if(this.alarmCollection[i].time === time ){
+                console.log('Уже присутствует звонок на это же время')
+            }
+        }
+    this.alarmCollection.push(new Ring(callback, time, true))
     }
 
     removeClock(time) {
-        this.alarmCollection = this.alarmCollection.time.filter(!time)
+        let existingRings = this.alarmCollection.filter(function (ring) {
+            return (ring.getTime() === time);
+        })
     }
 
     getCurrentFormattedTime() {
-        return new Date().getHours()+':'+ new Date().getMinutes();
+        let hh = new Date().getHours()
+        let mm = new Date().getMinutes()
+        if(hh<=9){
+            hh='0'+hh
+        }
+        if (mm<=9){
+            mm='0'+mm
+        }
+        return hh+':'+mm;
     }
+
     start() {
         if (this.intervalId) {
-            return this.intervalId;
-        } else {
-            this.intervalId = setInterval(this.alarmCollection.forEach(time === new Date(),this.alarmCollection.canCall==true), 1000)
-            if (this.alarmCollection.canCall) {
-                this.alarmCollection.canCall = false;
-                this.alarmCollection.callback();
-            }
+            return;
         }
+        this.intervalId = setInterval(this.runInterval(), 1000)
+    }
 
+    runInterval() {
+        let currentTime = this.getCurrentFormattedTime();
+
+        this.alarmCollection.forEach((ring) => {
+            if(ring.getTime()===currentTime && ring.getCanCall()===true){
+                ring.getCanCall(false)
+                ring.callback()
+            }
+        })
     }
 
     stop() {
@@ -41,11 +87,13 @@ class AlarmClock {
     }
 
     resetAllCalls() {
-        this.alarmCollection.forEach(this.alarmCollection.canCall = true)
+        this.alarmCollection.forEach((n)=> {
+            n.setCanCall(true)
+        })
     }
 
     clearAlarms() {
-        stop()
+        this.stop()
         this.alarmCollection = []
     }
 }
